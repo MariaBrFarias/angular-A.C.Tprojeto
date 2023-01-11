091cf9d39447e4526ba2ee2fa57263477d47cf93
MariaBrFarias <110403184+MariaBrFarias@users.noreply.github.com>
2023-01-10 22:16:29 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-A.C.Tprojeto
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-oag1ot)
---
+++ src/app/barradepesquisa/barradepesquisa.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-barradepesquisa',
+  templateUrl: './barradepesquisa.component.html',
+  styleUrls: ['./barradepesquisa.component.css']
+})
+export class BarradepesquisaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
